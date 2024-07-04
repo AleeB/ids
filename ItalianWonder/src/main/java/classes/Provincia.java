93ee1893a1e87a76a76b07115678787bd236d70d
AleeB <71321299+AleeB@users.noreply.github.com>
2024-07-04 16:52:09 +0200

    added provincia class

--- ItalianWonder/src/main/java/classes/Provincia.java
+++ ItalianWonder/src/main/java/classes/Provincia.java
@@ -1,16 +1,80 @@
 package classes;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * le province verranno caricate in modo statico
  */
 public class Provincia {
 
-	private String nomeProvincia;
-	private char Sigla;
+	//region Vars
 
-	public void provincia() {
-		// TODO - implement Provincia.provincia
-		throw new UnsupportedOperationException();
+	private String nomeProvincia;
+	private String sigla;
+
+	//endregion
+
+	//region Constructors
+
+	public Provincia(String nomeProvincia, String sigla) {
+		this.nomeProvincia = nomeProvincia;
+		this.sigla = sigla;
 	}
+
+	public Provincia() {
+		this.nomeProvincia = "";
+		this.sigla = "";
+	}
+	
+	//endregion
+
+	//region Properties
+
+	public String getNomeProvincia() {
+		return nomeProvincia;
+	}
+
+
+
+	public void setNomeProvincia(String nomeProvincia) {
+		this.nomeProvincia = nomeProvincia;
+	}
+
+
+
+	public String getSigla() {
+		return sigla;
+	}
+
+
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	//endregion
+
+	//region Methods
+
+	protected List<Provincia> getProvinceFromFile(){
+		String nomeFile = "files/Province.dat";
+		ArrayList<Provincia> prov = new ArrayList<Provincia>();
+		try(BufferedReader br = new BufferedReader(new FileReader(nomeFile))) {
+			String[] line;
+			while (br.readLine() != null) {
+				line = br.readLine().split(";");
+				prov.add(new Provincia(line[0], line[1]));
+			}
+			br.close();
+			return prov;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	//endregion
 
 }
