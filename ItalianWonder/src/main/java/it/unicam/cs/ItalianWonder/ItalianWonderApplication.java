e99ef7fa140225bac851c958bd12fdfa842e59da
AleeB <71321299+AleeB@users.noreply.github.com>
2024-08-30 15:28:31 +0200

    added postgresql dependency and check if it works

--- ItalianWonder/src/main/java/it/unicam/cs/ItalianWonder/ItalianWonderApplication.java
+++ ItalianWonder/src/main/java/it/unicam/cs/ItalianWonder/ItalianWonderApplication.java
@@ -2,12 +2,20 @@ package it.unicam.cs.ItalianWonder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ItalianWonderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItalianWonderApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello World";
 	}
 
 }
