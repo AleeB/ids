4bbb031626c86315b7fddeb5f37d5f450c2f2a2b
AleeB <71321299+AleeB@users.noreply.github.com>
2024-09-23 14:02:55 +0200

    added segnalazione' CRUD APIs

--- ItalianWonder/src/main/java/it/unicam/cs/ItalianWonder/repositories/SegnalazioneRepository.java
+++ ItalianWonder/src/main/java/it/unicam/cs/ItalianWonder/repositories/SegnalazioneRepository.java
@@ -1,7 +1,11 @@
 package it.unicam.cs.ItalianWonder.repositories;
 
 import it.unicam.cs.ItalianWonder.classes.Segnalazione;
+import it.unicam.cs.ItalianWonder.classes.enums.enumTipoSegnalazione;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SegnalazioneRepository extends JpaRepository<Segnalazione, Integer> {
+import java.util.List;
+
+public interface SegnalazioneRepository extends JpaRepository<Segnalazione, Long> {
+    List<Segnalazione> findByTipo(enumTipoSegnalazione tipoSegnalazione);
 }
